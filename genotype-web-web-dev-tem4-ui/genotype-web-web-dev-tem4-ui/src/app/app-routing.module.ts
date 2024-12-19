a3f6b0a1ba2fc5a718b25dc9f39e8800e1534f99
vigneshSivanesan <vignesh.sivanesan@softsuvae.com>
2024-12-19 11:24:11 +0530

    fix

--- genotype-web-web-dev-tem4-ui/genotype-web-web-dev-tem4-ui/src/app/app-routing.module.ts
+++ genotype-web-web-dev-tem4-ui/genotype-web-web-dev-tem4-ui/src/app/app-routing.module.ts
@@ -1,27 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AuthLayoutComponent} from "./layout/auth-layout/auth-layout.component";
-import {AuthGuard} from "./core/guards/auth.guard";
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: '',
-    loadChildren: () => import ('./layout/auth-layout/auth-layout.module').then(e => e.AuthLayoutModule),
+    loadChildren: () =>
+      import('./layout/auth-layout/auth-layout.module').then(
+        (e) => e.AuthLayoutModule
+      ),
   },
   {
     path: 'admin',
-    loadChildren: () => import ('./layout/admin-layout/admin-layout.module').then(e => e.AdminLayoutModule),
-    canActivate: [AuthGuard]
+    loadChildren: () =>
+      import('./layout/admin-layout/admin-layout.module').then(
+        (e) => e.AdminLayoutModule
+      ),
+    canActivate: [AuthGuard],
   },
   {
     path: '**',
-    loadChildren: () => import ('./layout/auth-layout/auth-layout.module').then(e => e.AuthLayoutModule)
+    loadChildren: () =>
+      import('./layout/auth-layout/auth-layout.module').then(
+        (e) => e.AuthLayoutModule
+      ),
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
